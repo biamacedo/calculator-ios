00120b2a112a8a97d208771519f1b38a17341535
Beatriz Macedo <bianet13@Beatrizs-Mac-mini.local>
2015-08-30 15:45:26 -0700

    Innit Commit - Video 3 - Programming Exercise 1

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,92 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var historyDisplay: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber = false
+    
+    var brain = CalculatorBrain()
+
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if digit == "." && display.text!.rangeOfString(".") != nil {
+            return
+        }
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+        updateHistory()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func operate(sender: UIButton) {
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
+        updateHistory()
     }
-
-
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
+        updateHistory()
+    }
+    
+    @IBAction func clear() {
+        userIsInTheMiddleOfTypingANumber = false
+        displayValue = 0
+        brain.clear()
+        updateHistory()
+    }
+    
+    @IBAction func backspace() {
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = dropLast(display.text!)
+            if display.text! == ""{
+                displayValue = 0
+            }
+        }
+    }
+    
+    @IBAction func invertSign() {
+        display.text = "\(displayValue * -1)"
+        if !userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            // Constants
+            switch display.text! {
+            case "π":
+                return M_PI
+            default:
+                return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+            }
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+    
+    func updateHistory() {
+        historyDisplay.text = brain.stackText()
+    }
 }
 
